7c28d67ee8b4d1a7fb2a5d822870fff8e9092439
Mekire <3ror@live.com>
2014-01-11 14:09:38 +0900

    Added sound trigger on hover.

--- button/button.py
+++ button/button.py
@@ -8,6 +8,7 @@ class Button(object):
         self.color = color
         self.function = function
         self.clicked = False
+        self.hovered = False
         self.hover_text = None
         self.clicked_text = None
         self.process_kwargs(kwargs)
@@ -15,14 +16,16 @@ class Button(object):
 
     def process_kwargs(self,kwargs):
         """Various optional customization you can change by passing kwargs."""
-        settings = {"hover_color" : None,
-                    "clicked_color" : None,
-                    "on_release" : True,
-                    "text" : None,
+        settings = {"text" : None,
                     "font" : pg.font.Font(None,16),
+                    "call_on_release" : True,
+                    "hover_color" : None,
+                    "clicked_color" : None,
                     "font_color" : pg.Color("white"),
                     "hover_font_color" : None,
-                    "clicked_font_color" : None,}
+                    "clicked_font_color" : None,
+                    "click_sound" : None,
+                    "hover_sound" : None}
         for kwarg in kwargs:
             if kwarg in settings:
                 settings[kwarg] = kwargs[kwarg]
@@ -44,24 +47,40 @@ class Button(object):
     def check_event(self,event):
         """The button needs to be passed events from your program event loop."""
         if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-            if self.rect.collidepoint(event.pos):
-                self.clicked = True
-                if not self.on_release:
-                    self.function()
+            self.on_click(event)
         elif event.type == pg.MOUSEBUTTONUP and event.button == 1:
-            if self.clicked and self.on_release:
+            self.on_release(event)
+
+    def on_click(self,event):
+        if self.rect.collidepoint(event.pos):
+            self.clicked = True
+            if not self.call_on_release:
                 self.function()
-            self.clicked = False
+
+    def on_release(self,event):
+        if self.clicked and self.call_on_release:
+            self.function()
+        self.clicked = False
+
+    def check_hover(self):
+        if self.rect.collidepoint(pg.mouse.get_pos()):
+            if not self.hovered:
+                self.hovered = True
+                if self.hover_sound:
+                    self.hover_sound.play()
+        else:
+            self.hovered = False
 
     def update(self,surface):
         """Update needs to be called every frame in the main loop."""
         color = self.color
         text = self.text
+        self.check_hover()
         if self.clicked and self.clicked_color:
             color = self.clicked_color
             if self.clicked_font_color:
                 text = self.clicked_text
-        elif self.hover_color and self.rect.collidepoint(pg.mouse.get_pos()):
+        elif self.hovered and self.hover_color:
             color = self.hover_color
             if self.hover_font_color:
                 text = self.hover_text
--- example.py
+++ example.py
@@ -22,7 +22,9 @@ ORANGE = (255,180,0)
 BUTTON_STYLE = {"hover_color" : BLUE,
                 "clicked_color" : GREEN,
                 "clicked_font_color" : BLACK,
-                "hover_font_color" : ORANGE}
+                "hover_font_color" : ORANGE,
+                "call_on_release" : False,
+                "hover_sound" : pg.mixer.Sound("blipshort1.wav")}
 
 
 class Control(object):
